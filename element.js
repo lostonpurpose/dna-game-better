bf3b3302ca79d7becbb7f7104f4054847573f3ec
Will VanderWyden <vanderwyden@gmail.com>
2025-04-13 15:14:03 +0900

    working out child creation logic

--- element.js
+++ element.js
@@ -1,14 +1,16 @@
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export class Element {
     constructor(name, colorDna) {
         this.name = name;
         this.colorDna = colorDna;
     }
 
-    function capitalize(str) {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }
+
 
     checkDna() {
-        console.log(`${this.name}'s color DNA alleles are ${this.colorDna.slot1.toUpperCase()} and ${this.colorDna.slot2}`)
+        console.log(`${this.name}'s color DNA alleles are ${capitalize(this.colorDna.slot1)} and ${capitalize(this.colorDna.slot2)}`)
     }
 }
--- script.js
+++ script.js
@@ -10,3 +10,24 @@ const adam = new Element("Adam", colorDom);
 const eve = new Element("Eve", colorRec);
 
 adam.checkDna();
+eve.checkDna();
+
+function generateNumber() {
+    let chance = Math.floor((Math.random() * 2) + 1);
+    // console.log(chance);
+};
+
+generateNumber();
+
+function generateSlots(parent1, parent2) {
+    const bioChance1 = generateNumber();
+    bioChance1 === 1 ? parent1.colorDna.slot1 : parent1.colorDna.slot2
+    const bioChance2 = generateNumber();
+    bioChance2 === 1 ? parent2.colorDna.slot1 : parent2.colorDna.slot2
+};
+
+
+
+const abel = new Element("Abel", generateSlots(adam, eve))
+
+console.log(abel);
